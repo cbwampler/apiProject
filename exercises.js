e5b9802bfb573f17c3ed3a3944824f588092e1a5
cbwampler <cbwampler@outlook.com>
2019-11-24 16:42:00 -0500

    update as of Nov 24

--- exercises.js
+++ exercises.js
@@ -1,7 +1,7 @@
 baseURL= "https://trackapi.nutritionix.com/v2/natural/exercise";
 const appID = "0ea8257f";
 const appKey = "8d3608f6f98eaaede5c6c69d34f87008";
-const section = document.querySelector('section');
+const section = document.querySelector('ul');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
 let input = document.getElementById('search');
@@ -11,6 +11,7 @@ searchForm.addEventListener('submit',getExercise);
 function getExercise(e){
     e.preventDefault();
 let exercise = input.value;
+
 fetch(baseURL,{
     method:"POST",
     headers: new Headers({
@@ -28,22 +29,21 @@ fetch(baseURL,{
     })
 })
 .then(result => result.json())
-.then(function(response){
-    console.log(response)
-    //console.log(response.exercises[0].duration_min)
-    displayResults(response);
+.then(json => {
+    displayExercises(json);
 })
 }
-function displayResults(response){
-    let exercises = response.exercises;
 
-    if (exercises.length === 0){
-        console.log("No results");
-    }else{
-        for(let i = 0; i < exercises.length; i++){
-        let exercise = document.createElement(exercise);
+function displayExercises(json){
+    let exercises = json.exercises;
+    console.log(exercises);
 
-        section.appendChild(exercise);
+    if(exercises.length === 0) {
+        console.log("No Resuts");
+    }else {exercises.forEach(e =>{
+        let exercises = document.createElement('li');
+        exercises.innerHTML = `${e.name} burned ${Math.round(e.nf_calories)} calories in ${e.duration_min} minutes`;
+        section.appendChild(exercises);
+    })
     }
-}
 };
